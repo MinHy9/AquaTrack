efc32ade1375d78cd9f29f6d2b3f30b2f60a6767
ymh0977 <144010663+ymh0977@naver.com>
2025-06-01 19:29:22 +0900

    로그인 유지 체크박스 기능 작동하도록

--- frontend/js/nav.js
+++ frontend/js/nav.js
@@ -1,13 +1,15 @@
 // 로그인시 로그아웃, 어항등록 보이게 하기
 export function renderNavbar() {
     const nav = document.getElementById('nav-links');
-    const isLoggedIn = localStorage.getItem('loggedIn') === 'true';
+    const isLoggedIn =
+        localStorage.getItem('loggedIn') === 'true' ||
+        sessionStorage.getItem('loggedIn') === 'true';
 
     if (nav) {
         if (isLoggedIn) {
             nav.innerHTML = `
                 <a href="index.html" class="text-primary hover:text-primary/80">대시보드</a>
-                <a href="register.html" class="text-primary hover:text-primary/80">어항 등록</a>
+                <a href="aquarium-register.html" class="text-primary hover:text-primary/80">어항 등록</a>
                 <a href="#" class="text-primary hover:text-primary/80" id="logout-btn">로그아웃</a>
             `;
         } else {
